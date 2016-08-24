73d062049489d3f16645b80882a43b91ee772e4a
Miriam Cortes <mcortes.umiami@gmail.com>
2016-08-24 13:02:33 -0700

    DeafGrandma example coded in

--- pseudocode.rb
+++ pseudocode.rb
@@ -1,6 +1,6 @@
-##################
-### OBJECTIVE:  ##
-##################
+###################
+###  OBJECTIVE  ###
+###################
 # Write a Deaf Grandma program. Whatever you say to grandma (whatever you type in), she should respond with  HUH?!  SPEAK UP, SONNY!, unless you shout it (type in all capitals). If you shout, she can hear you (or at least she thinks so) and yells back, NO, NOT SINCE 1938!
 
 #INPUT:
@@ -17,25 +17,34 @@
 ### PSEUDOCODE EXAMPLE:  ##
 ###########################
 
-# PROGRAM DeafGrandma
-#     Keep LOOPING to get more input
+#PROGRAM deaf_grandma method
+def deaf_grandma
+  puts "OIGO??"
+#  Keep LOOPING to get more input
+  while true
+#    Ask what to say to grandma
+    user_input = gets.chomp
+#      IF we said "BYE"
+    if user_input == "BYE"
+#        stop asking for more input and leave
+      break
+#      END
+    end
 #
-#         Ask what to say to grandma
-#
-#         IF we said "BYE"
-#             stop asking for more input and leave
-#         END
-#
-#         IF what I just said is too quiet
-#             Grandma responds "HUH!? SPEAK UP, SONNY!"
-#         ELSE
-#             Grandma responds "NO, NOT SINCE 1938!"
-#         END
-#
-#     END
-# END
+#      IF I'm shouting
+    if user_input.upcase == user_input
+#        Grandma responds "NO, NOT SINCE 1938!"
+      puts "NO, TU PAPA NO ESTA AQUI!"
+#      ELSE what I just said is too quiet
+    else
+#        Grandma responds "HUH!? SPEAK UP, SONNY!"
+      puts "QUE??? NO TE OIGO!"
+#      END
+    end
+  end#  END
+end#END
 
-
+deaf_grandma
 
 
 ###########################
